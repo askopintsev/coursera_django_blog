2f5e4e96579f2ac0fa12d7dfbf3fc275a2810093
Alex <milleniumfalcon_@mail.ru>
2020-10-29 00:55:07 +0300

    Template task part 1 completed

--- template/views.py
+++ template/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
+
 def echo(request):
-    pass
+    method = request.method.lower()
+    header = request.META.get('HTTP_X_PRINT_STATEMENT', 'empty')
+    if method == 'get':
+        params = request.GET.copy()
+    elif method == 'post':
+        params = request.POST.copy()
+
+    content = {"method": method, "params": params, "statement": header}
+
+    return render(request, 'echo.html', context=content, status=200)
 
 
 def filters(request):
