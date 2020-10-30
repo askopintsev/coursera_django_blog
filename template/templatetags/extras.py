04ca5a21563d5233c41366c6515a740bd693aa0c
Alex <milleniumfalcon_@mail.ru>
2020-10-30 23:03:41 +0300

    Templates and filters task completed

--- template/templatetags/extras.py
+++ template/templatetags/extras.py
@@ -1,1 +1,19 @@
 from django import template
+
+register = template.Library()
+
+
+@register.filter(name='inc')
+def inc(digit, adding):
+    return int(digit) + int(adding)
+
+
+@register.simple_tag
+def division(a, b, to_int=False):
+    a = int(a)
+    b = int(b)
+    if to_int:
+        result = int(a / b)
+    else:
+        result = a / b
+    return result
